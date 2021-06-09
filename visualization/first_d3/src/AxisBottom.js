d4e0a52835b3a1b12903d72322c78c69dfde222e
reama623 <83913707+reama623@users.noreply.github.com>
2021-06-09 09:36:28 +0900

    Stylized Line Chart
    * grouped left, bottom tick list

--- visualization/first_d3/src/AxisBottom.js
+++ visualization/first_d3/src/AxisBottom.js
@@ -1,10 +1,14 @@
 export const AxisBottom = ({ xScale, innerHeight, axisTickFormat }) => {
-  return xScale.ticks().map((tick, i) => (
-    <g className="tick" key={i} transform={`translate(${xScale(tick)},0)`}>
-      <line key={i} y2={innerHeight} stroke="black" />
-      <text y={innerHeight + 3} dy=".71em" style={{ textAnchor: "middle" }}>
-        {axisTickFormat(tick)}
-      </text>
+  return (
+    <g>
+      {xScale.ticks().map((tick, i) => (
+        <g className="tick" key={i} transform={`translate(${xScale(tick)},0)`}>
+          <line key={i} y2={innerHeight} />
+          <text y={innerHeight + 3} dy=".71em" style={{ textAnchor: "middle" }}>
+            {axisTickFormat(tick)}
+          </text>
+        </g>
+      ))}
     </g>
-  ));
+  );
 };
